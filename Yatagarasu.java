4f945a54683c6fb43675ab73a0d1b65ce324bbbc
kohta <15.kohta@gmail.com>
2018-09-09 11:43:13 +0900

    Yatagarasu.java created

---
+++ Yatagarasu.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Yatagarasu extends Monster implements Bird {
+
+	@Override
+	public void run() {
+		System.out.println("八咫烏は逃げ出した！");
+	}
+
+	@Override
+	int attack(int hp) {
+		Random rnd = new Random();
+		int i = rnd.nextInt(100) + 1;
+		hp -= i;
+		System.out.println("八咫烏の攻撃" + i + "のダメージ！");
+		return hp;
+	}
+
+	@Override
+	void dead(int hp) {
+		if(hp <= 0) {
+			System.out.println("八咫烏は倒れた...");
+		}
+	}
+
+}
