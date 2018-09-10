74e7502d4557d50b0398ba7ebd220b1e6601bd97
ninomiya <ninomiya.1106cm@gmail.com>
2018-09-10 13:07:11 +0900

    add Wand

--- Wand.java
+++ Wand.java
@@ -6,7 +6,7 @@ public class Wand extends Weapon {
 		Random rnd = new Random();
 		int i = rnd.nextInt(100) + 1;
 		hp -= i;
-		System.out.println("まるふぉい！");
+		System.out.println("杖で攻撃!");
 		return hp;
 	}
 }
