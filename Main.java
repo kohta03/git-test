e654396b8e5b218e22fb815ca758788853d36632
kohta <15.kohta@gmail.com>
2018-09-10 14:18:48 +0900

    Main remake

--- Main.java
+++ Main.java
@@ -6,12 +6,38 @@ public class Main {
 		Yatagarasu y1 = new Yatagarasu();
 		Yatagarasu y2 = new Yatagarasu();
 		Zombie zombie = new Zombie();
+		Sword sword = new Sword("エクスカリバー", 30);
 
-		//攻撃
-		for(; y2.getHp() > 0;) {
-			int damage = zombie.attack(y2.getHp());
+		//八咫烏同士の攻撃
+		for(; y1.getHp() > 0;) {
+			int damage = y2.attack(y1.getHp());
+			y1.setHp(damage);
+			y1.dead(y1.getHp());
+		}
+
+		//インスタンス化
+		Hero hero = new Hero(30, "藤井");
+		Wizard wizard = new Wizard(25, "伊藤");
+
+		//素手殴り
+		int damage = hero.attack(y2.getHp());
+		y2.setHp(damage);
+		y2.dead(y2.getHp());
+
+		//剣装備
+		hero.quipmentSword(sword);
+
+		//剣殴り
+		for(; y2.getHp() > 0 && wizard.getHp() > 0;) {
+			//heroの攻撃
+			damage = sword.attack(y2.getHp());
 			y2.setHp(damage);
 			y2.dead(y2.getHp());
+
+			//wizardの被弾
+			damage = zombie.attack(wizard.getHp());
+			wizard.setHp(damage);
+			wizard.dead(wizard.getHp());
 		}
 	}
 }
