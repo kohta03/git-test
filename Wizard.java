5a5394dcd21748c9e4159a849eb088f482594ff5
kohta <15.kohta@gmail.com>
2018-09-10 13:50:33 +0900

    Wizard.java created

---
+++ Wizard.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class Wizard extends Character implements Human {
+
+	private Wand wand;
+
+	Wizard(int lv, String name) {
+		super(lv, name);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("魔法使い" + super.getName() +  "は逃げ出した");
+	}
+
+	@Override
+	int attack(int hp) {
+		Random rnd = new Random();
+		int i = rnd.nextInt(5) + 1;
+		hp -= i;
+		System.out.println(super.getName() + "の攻撃" + i + "のダメージ！");
+		return hp;
+	}
+
+	@Override
+	void dead(int hp) {
+		if(hp <= 0) {
+			System.out.println(super.getName() + "は倒れた...");
+		}
+	}
+
+	public void quipmentWand(Wand wand) {
+		this.wand = wand;
+		System.out.println(this.getName() + "は" + wand.getName() + "を装備した");
+	}
+}
