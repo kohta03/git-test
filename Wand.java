41c4f09ebaeaeaaed9f37f44e0784bc46d88624f
ninomiya1106 <42921519+ninomiya1106@users.noreply.github.com>
2018-09-10 14:18:11 +0900

    Update Wand.java

--- Wand.java
+++ Wand.java
@@ -1,12 +1,26 @@
 import java.util.Random;
 
 public class Wand extends Weapon {
+	public Wand(String name, int power) {
+		super(name, power);
+	}
+	
 	int attack(int hp){
-		int hp;
-		Random rnd = new Random();
-		int i = rnd.nextInt(100) + 1;
-		hp -= i;
-		System.out.println("杖で攻撃!");
+		hp -= this.getPower();
+		System.out.println("杖で攻撃！" + this.getPower() + "のダメージを与えたぞ！");
+		return hp;
+	}
+	
+	int Throw(int hp) {
+		Rundom rnd = new Random();
+		int i = rnd.nextInt(2) + 1;
+		
+		if(i % 2 == 2){
+			 System.out.println("あ、こいつ杖を投げたぞ！" + this.getPower() + "のダメージを与えたぞ！");
+			 hp -= this.getPower();
+		} else {
+			System.out.println("杖が当たらなかった。");
+		}
 		return hp;
 	}
 }
