055b348739a306b94ce233b994b555459b5069ff
kohta <15.kohta@gmail.com>
2018-09-10 13:56:56 +0900

    Hero.java remake

--- Hero.java
+++ Hero.java
@@ -1,33 +1,36 @@
 import java.util.Random;
 
-	public class Hero extends Character implements Human {
+public class Hero extends Character implements Human {
 
-		 String name;
-		 int lv;
+	private Sword sword;
 
-		Hero(){
-            this.name = super.getName();
-            this.lv = super.getLv();
-		}
-		@Override
-		public void run() {
-			System.out.println("勇者" + super.getName() +"は逃げ出した！");
-		}
-
-		@Override
-		int attack(int hp) {
-			Random rnd = new Random();
-			int i = rnd.nextInt(100) + 1;
-			hp -= i;
-			System.out.println("勇者" + super.getName() + "の攻撃" + i + "のダメージ！");
-			return hp;
-		}
-
-		@Override
-		void dead(int hp) {
-			if(hp <= 0) {
-				System.out.println("勇者" + super.getName() + "は倒れた...");
-			}
-		}
-
+	Hero(int lv, String name) {
+		super(lv, name);
 	}
+
+	@Override
+	public void run() {
+		System.out.println("勇者" + super.getName() +  "は逃げ出した");
+	}
+
+	@Override
+	int attack(int hp) {
+		Random rnd = new Random();
+		int i = rnd.nextInt(10) + 1;
+		hp -= i;
+		System.out.println(super.getName() + "の攻撃" + i + "のダメージ！");
+		return hp;
+	}
+
+	@Override
+	void dead(int hp) {
+		if(hp <= 0) {
+			System.out.println(super.getName() + "は倒れた...");
+		}
+	}
+
+	public void quipmentSword(Sword sword) {
+		this.sword = sword;
+		System.out.println(this.getName() + "は" + sword.getName() + "を装備した");
+	}
+}
