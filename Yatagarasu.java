59a8fb27a3265b3d3e61e944abbf79b64b7f1e65
abe <oic.a.tsunobe@gmail.com>
2018-09-11 09:56:20 +0900

    fry method add

--- Yatagarasu.java
+++ Yatagarasu.java
@@ -6,6 +6,10 @@ public class Yatagarasu extends Monster implements Bird {
 	public void run() {
 		System.out.println("八咫烏は逃げ出した！");
 	}
+	@Override
+	public void fry() {
+		System.out.println("八咫烏は飛び出した！");
+	}
 
 	@Override
 	int attack(int hp) {
