f4b51aa935df130693b8c5e4abbd33c018e95e9e
kohta <15.kohta@gmail.com>
2018-09-11 10:28:36 +0900

    Hero added

--- Hero.java
+++ Hero.java
@@ -5,7 +5,11 @@ public class Hero extends Character implements Human {
 	private Sword sword;
 
 	Hero(int lv, String name) {
-		super(lv, name);
+		super(name, lv);
+	}
+
+	Hero() {
+		super("ななしさん", 1);
 	}
 
 	@Override
@@ -23,14 +27,30 @@ public class Hero extends Character implements Human {
 	}
 
 	@Override
-	void dead(int hp) {
+	int dead(int hp) {
 		if(hp <= 0) {
 			System.out.println(super.getName() + "は倒れた...");
 		}
+		return 0;
 	}
 
 	public void quipmentSword(Sword sword) {
 		this.sword = sword;
 		System.out.println(this.getName() + "は" + sword.getName() + "を装備した");
+	}
+
+	@Override
+	public void talk() {
+		System.out.println("今日もいい天気ですね");
+	}
+
+	//レベルアップ
+	public int levelUp(int exp, int lv, int hp) {
+		if(this.getExpPoint() + exp >= 100) {
+			lv += 1;
+			this.setHp(100);
+			return lv;
+		}
+		return lv;
 	}
 }
