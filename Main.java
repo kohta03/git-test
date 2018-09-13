7ddef96bb557add5b44b27447dcfdb63d306a4dd
kohta <15.kohta@gmail.com>
2018-09-13 10:28:03 +0900

    Main

--- Main.java
+++ Main.java
@@ -1,72 +1,42 @@
-import java.util.Random;
-import java.util.Scanner;
+public class Main {
 
-public class Hero extends Character implements Human {
+	public static void main(String[] args) {
 
-	private Sword sword;
+		 //インスタンス化
+		Yatagarasu y1 = new Yatagarasu("八咫烏A", 100);
+		Yatagarasu y2 = new Yatagarasu("八咫烏B", 100);
+		Zombie zombie = new Zombie("ゾンビ", 100);
+		Battle battle = new Battle();
 
-	Hero(String name, int lv) {
-		super(name, lv);
+		//八咫烏同士の攻撃
+		battle.mvmBattle(y1, y2);
+
+		//インスタンス化
+		Hero hero = new Hero("藤井", 30);
+		Wizard wizard = new Wizard("伊藤", 25);
+
+		//素手殴り
+		y2.setHp(hero.attack(y2.getHp(), hero.isFlg()));
+		y2.dead(y2.getHp());
+
+
+		//剣装備
+		hero.setFlg(hero.quipmentSword());
+
+		//ヒーローと八咫烏の戦い
+		battle.pvmBatlle(hero, y2);
+
+		//杖装備
+		wizard.setFlg(wizard.quipmentWand());
+
+		//wizardとゾンビの戦い
+		battle.pvmBatlle(wizard, zombie);
+
+		//スーパーヒーローのインスタンス化
+		SuperHero sh = new SuperHero("藤原", 40);
+
+		//スーパーヒーローとヒーローの戦い
+		battle.pvpBattle(hero, sh);
+
 	}
-
-	Hero() {
-		super("ななしさん", 1);
-	}
-
-	@Override
-	public void run() {
-		System.out.println("勇者" + super.getName() +  "は逃げ出した");
-	}
-
-	@Override
-	int attack(int hp, boolean flg) {
-		if(flg) {
-			hp = sword.attack(hp);
-		}else {
-			Random rnd = new Random();
-			int i = rnd.nextInt(10) + 1;
-			hp -= i;
-			System.out.println(super.getName() + "の攻撃" + i + "のダメージ！");
-		}
-		return hp;
-	}
-
-	@Override
-	int dead(int hp) {
-		if(hp <= 0) {
-			System.out.println(super.getName() + "は倒れた...");
-		}
-		return 0;
-	}
-
-	//剣装備
-	public boolean quipmentSword() {
-
-		//装備品の名前、強さを入力
-		System.out.println("装備する剣と強さを入力してください");
-		Scanner scanner = new Scanner(System.in);
-		String s_name = scanner.next();
-		int s_power = Integer.parseInt(scanner.next());
-
-		Sword sword = new Sword(s_name, s_power);
-		this.sword = sword;
-		System.out.println(this.getName() + "は" + sword.getName() + "を装備した");
-		return true;
-	}
-
-	@Override
-	public void talk() {
-		System.out.println("今日もいい天気ですね");
-	}
-
-	//レベルアップ
-	public int levelUp(int exp, int lv, int hp) {
-		if(this.getExpPoint() + exp >= 100) {
-			lv += 1;
-			this.setHp(this.getMaxHp());
-			return lv;
-		}
-		return lv;
-	}
-
 }
