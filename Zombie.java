90234d3920db98a9ff4b94bc8d0e453e85e5c99d
lowson <iwase2248@gmail.com>
2018-09-13 14:00:12 +0900

    Zombie テスト修正しました

--- Zombie.java
+++ Zombie.java
@@ -3,39 +3,53 @@ import java.util.Random;
 public class Zombie extends Monster implements Human {
 
 
-	//逃げ出す
+	//Zombieのコンストラクタ
+	Zombie(String name, int hp) {
+		super(name, hp);
+	}
+
+	Zombie(String name) {
+		super(name, 100);
+	}
+
+	Zombie() {
+		super("zombie", 100);
+	}
+
+
+	//ゾンビ逃げ出す
 	@Override
 	public void run() {
 		System.out.println("ゾンビは逃げ出した");
 	}
 
-	//かみつく
+	/*攻撃*/
 	@Override
-	public int attack(int hp) {
+	int attack(String name,int hp) {
+		//ランダムでダメージを算出
 		Random rnd = new Random();
 		int i = rnd.nextInt(100) + 1;
 		hp -= i;
-		System.out.println("ゾンビがかみついた！！" + i + "のダメージ！");
+		System.out.println(name + "の攻撃!" + i + "のダメージ！");
 		return hp;
 	}
 
-	//死ぬ
+	//ゾンビ死ぬ
 	@Override
 	public int dead(int hp) {
+
+		//Hpが-100以下になったら死ぬ
 		if(hp <= -100) {
 			System.out.println("ゾンビは倒れた...");
-			System.out.println("経験値" + this.getExp() + "を手に入れた");
-
 			return this.getExp();
 		}
 		return 0;
 	}
 
-	//しゃべる
+	//ゾンビ呻く
 	@Override
 	public void talk() {
-		System.out.println("ｱｧｱｱｱ～～");
-
+		System.out.println("ｱｧｱｧｧｧｧ～～");
 	}
 
 }
