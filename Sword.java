a694e113fd17836df71efaba33725d1a9c44b9cf
kohta <15.kohta@gmail.com>
2018-09-13 14:07:17 +0900

    sword test change

--- Sword.java
+++ Sword.java
@@ -1,30 +1,40 @@
 import java.util.Random;
 
 public class Sword extends Weapon {
-	public Sword(String name, int power) {
-		// 親クラスの変数等を使う
-		super(name, power);
-	}
 
-	@Override
-	int attack( int hp ){
+        public Sword(String name, int power) {
+                // 親クラスの変数等を使う
+                super(name, power);
+        }
 
-		hp -= this.getPower();
-		System.out.println("飛天御剣流奥義'龍槌閃'!!!" + this.getPower() + "のダメージを与えたぞ！");
-		return hp;
-	}
+        public Sword() {
+        	super("木の棒", 10);
+        }
 
-	@Override
-	int Throw(int hp) {
-		Random rnd = new Random();
-		int i = rnd.nextInt(2) + 1;
+        @Override
+        int attack( int hp ){
+                hp -= this.getPower();
+                System.out.println(this.getName() + "飛天御剣流奥義'龍槌閃'!!!" + this.getPower() + "のダメージを与えたぞ！");
+                return hp;
+        }
 
-		if( i%2 == 2) {
-			System.out.println("あ、こいつ剣を投げたぞ！" + this.getPower() + "のダメージを与えたぞ");
-			hp -= this.getPower();
-		} else {
-			System.out.println("武器投げたけど当たらなかった。");
-		}
-		return hp;
-	}
+        @Override
+        int Throw(int hp, Character hero) {
+
+        	if(hero.isFlg() == false) {
+        		System.out.println("投げる武器が見つからなかった...");
+        		return hp;
+        	}else {
+        		Random rnd = new Random();
+                int i = rnd.nextInt(2) + 1;
+
+                if( i%2 == 2) {
+                	System.out.println("あ、こいつ" + this.getName() + "を投げたぞ！" + this.getPower() + "のダメージを与えたぞ");
+                    hp -= this.getPower();
+                } else {
+                    System.out.println("武器投げたけど当たらなかった。");
+                }
+                return hp;
+        	}
+        }
 }
