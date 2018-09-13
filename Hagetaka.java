65be522e5293d5f64b81804d9f167b0af2338055
lowson <iwase2248@gmail.com>
2018-09-13 13:14:56 +0900

    Hagetaka.javaの修正しました

--- Hagetaka.java
+++ Hagetaka.java
@@ -4,37 +4,50 @@ import java.util.Random;
 public class Hagetaka extends Monster implements Bird {
 
 
+	/***
+	 * コンストラクタ
+	 * @param name
+	 * @param hp
+	 */
 	Hagetaka(String name, int hp) {
 		super(name, hp);
 	}
 
-	Hagetaka() {
-		super("ハゲタカ", 200);
+	Hagetaka(String name) {
+		super(name, 100);
 	}
+
+	Hagetaka() {
+		super("ハゲタカ", 100);
+	}
+
+
 
 	@Override
 	public void run() {
-		System.out.println("ハゲタカは逃げ出した！");
+		System.out.println(this.getName() + "は逃げ出した！");
 	}
 
 	@Override
 	public void fly() {
-		System.out.println("ハゲタカは飛んだ");
+		System.out.println(this.getName() + "は飛んだ");
 	}
 
+
+	/*攻撃*/
 	@Override
-	int attack(int hp) {
+	int attack(String name,int hp) {
 		Random rnd = new Random();
 		int i = rnd.nextInt(100) + 1;
 		hp -= i;
-		System.out.println("ハゲタカの攻撃" + i + "のダメージ！");
+		System.out.println(name + "の攻撃" + i + "のダメージ！");
 		return hp;
 	}
 
 	@Override
 	int  dead(int hp) {
 		if(hp <= 0) {
-			System.out.println("ハゲタカは倒れた...");
+			System.out.println(this.getName() + "は倒れた...");
 			return getExp();
 		}
 		return 0;
